cf604704d374c4d470e4e350fc1049246e9bfd64
EliAlfVGabriel <130845207+EliAlfVGabriel@users.noreply.github.com>
2025-02-16 12:33:09 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchMovies from "./SearchMovies";
+import DynamicList from "./watchlist";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Movie Search</h1>
       <SearchMovies /> {/*renders the search bar and movie results*/}
+      <DynamicList /> {/*renders the dynamic list used for the watchlist*/}
     </div>
   );
 }
