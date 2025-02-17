c5c2b8bb78216bab1562e6de79eee7fb3d0733f2
DmendezJ <damian.mendez@ku.edu>
2025-02-16 19:11:54 -0600

    Some small additional comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Authors: Sophia, Eli, Damian, Matthew and Abraham
 // Date: 2/13/25
 // Last Modified: 2/16/25
-// Purpose: Handles application routing
+// Purpose: Handles application routing and rendering
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -9,12 +9,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); //Creates root, where all will be rendered.
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode> // Renders our App element in strict mode
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
