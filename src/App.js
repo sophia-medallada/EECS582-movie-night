25ec26c450cb845f8eb19ef409333ff36fa87fdc
Matthew Sullivan <matthewsullivan@ku.edu>
2025-04-17 16:42:47 -0500

    updated app.js to use settings and login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-// Authors: Sophia, Eli, Damian, Matthew and Abraham
-// Date: 2/13/25
-// Last Modified: 4/7/25
-// Purpose: Combines all the seperate application portions and bundles them together into a final render
-
-//import logo from './logo.svg';
-
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Container, AppBar, Toolbar, Button, Box } from '@mui/material';
-import ThemeToggleButton from './ThemeToggle';
+import {
+  Container, AppBar, Toolbar, Button, Box
+} from '@mui/material';
 
+import ThemeToggleButton from './ThemeToggle';
 import SearchMovies from "./components/SearchMovies";
 import SchedulePage from "./components/SchedulePage";
 import SnackList from "./components/RecSnacks";
-
+import LoginPage from "./components/LoginPage";
+import SettingsDrawer from "./components/SettingsDrawer";
 
 import './styles/App.css';
 
-
 function App() {
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
   return (
     <Router>
       <Container>
@@ -30,6 +27,12 @@ function App() {
             <Button color="inherit" component={Link} to="/schedule">
               Schedule
             </Button>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" onClick={() => setSettingsOpen(true)}>
+              Settings
+            </Button>
             <Box flexGrow={1} />
             <ThemeToggleButton />
           </Toolbar>
@@ -39,13 +42,15 @@ function App() {
           <Routes>
             <Route path="/search" element={<SearchMovies />} />
             <Route path="/schedule" element={<SchedulePage />} />
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
         </Box>
-        <SnackList/>
-      </Container>
+
+        <SnackList />
+        <SettingsDrawer open={settingsOpen} onClose={() => setSettingsOpen(false)} />
+        </Container>
     </Router>
   );
 }
 
 export default App;
-
